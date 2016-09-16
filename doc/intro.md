26e19b232f3dc60bb73f5e8f2c0efc8362c60a0a
ButenkoMS <gtalk@butenkoms.space>
2016-09-16 16:21:27 +0300

    Initial Lein Template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dribbble-experiments
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dribbble_experiments/core_test.clj
@@ -0,0 +1,7 @@
+(ns dribbble-experiments.core-test
+  (:require [clojure.test :refer :all]
+            [dribbble-experiments.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
