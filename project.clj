e41ff258d56f69fedfcf6e91e19f3d28c837abbb
ButenkoMS <gtalk@butenkoms.space>
2016-09-17 14:20:41 +0300

    Dribble crawler
    Task is done

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject dribbble-experiments "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "Dribble API experiments"
+  :url "https://github.com/FI-Mihej/Dribbble-Experiments"
+  :license {:name "GNU GENERAL PUBLIC LICENSE v.3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [ [org.clojure/clojure "1.8.0"]
+                  [clj-http "2.0.0"]
+                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot dribbble-experiments.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dribbble_experiments/core.clj
+++ src/dribbble_experiments/core.clj
@@ -1,7 +1,105 @@
 (ns dribbble-experiments.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clojure.string :as st]
+            [clojure.data.json :as json]))
+
+(def endpoint "https://api.dribbble.com/v1/")
+(def test-user "louiethelowe")
+(def test-access-token "")
+
+(defonce app-state (atom {:requests-count 0}))
+
+(defn dr-api [path token]
+  (do
+    (Thread/sleep 1000)
+    (swap! app-state assoc :requests-count (inc (get (deref app-state) :requests-count)))
+    (println (str "<< Request #" (:requests-count (deref app-state)) " >>"))
+    (json/read-str (:body (client/get (st/join "" [endpoint path]) {:insecure? true
+                                                                    :force-redirects true
+                                                                    :headers {"Authorization" (st/join "" ["Bearer " token])}})))))
+
+(defn username-and-name-by-user-id [user-id token]
+  (let [user-info (dr-api (st/join "" ["users/" user-id]) token)]
+    [(get user-info "username") (get user-info "name")]))
+
+(defn followers-ids [user-id token]
+  (let [user-followers (dr-api (st/join "" ["users/" user-id "/followers"]) token)]
+    (vec (map (fn [follower-info] (get (get follower-info "follower") "id")) user-followers))))
+
+(defn shots-ids [user-id token]
+  (let [user-shots (dr-api (st/join "" ["users/" user-id "/shots"]) token)]
+    (vec (map (fn [shot-info] (get shot-info "id")) user-shots))))
+
+(defn shots-likers-ids [shot-id token]
+  (let [shot-likers (dr-api (st/join "" ["shots/" shot-id "/likes"]) token)]
+    (vec (map (fn [liker-info] (get (get liker-info "user") "id")) shot-likers))))
+
+(defn find-top-likers-ids [user-name number-of-top-likers token]
+  (let [user-info (dr-api (st/join "" ["users/" user-name]) token)
+        user-id (get user-info "id")
+        user-followers (followers-ids user-id token)
+        all-shots-ids (flatten (vec (map (fn [follower-id] (shots-ids follower-id token)) user-followers)))
+        all-likers-ids-flat (remove nil? (flatten (map (fn [shot-id] (shots-likers-ids shot-id token)) all-shots-ids)))
+        likes-per-user-id (frequencies all-likers-ids-flat)
+        top-likers-ids (take number-of-top-likers (sort-by val > likes-per-user-id))
+        ]
+    (println "")
+    (println "User Info:")
+    (println user-info)
+    (println "")
+    (println "Followers of the User:")
+    (println user-followers)
+    (println "")
+    (println "Shots made by the Followers :")
+    (println all-shots-ids)
+    (println "")
+    (println "User IDs of the all users who liked Shots:")
+    (println all-likers-ids-flat)
+    (println "")
+    (println "Number of Likes per user ID of the all users who liked Shots:")
+    (println likes-per-user-id)
+    (println "")
+    (println (str "User IDs and number of Likes of the Top " number-of-top-likers " Likers:"))
+    (println top-likers-ids)
+    top-likers-ids))
+
+(defn find-top-likers-names [user-name number-of-top-likers token]
+  (let [top-likers-ids (find-top-likers-ids user-name number-of-top-likers token)
+        top-likers-names (vec (map (fn [user-id-count] (username-and-name-by-user-id (get user-id-count 0) token)) (seq top-likers-ids)))]
+    (println "")
+    (println "Names of the Top Likers:")
+    (println top-likers-names)
+    top-likers-names))
+
+(defn print-top-likers [user-name number-of-top-likers token]
+  (let [top-likers-names (find-top-likers-names user-name number-of-top-likers token)]
+    (println "")
+    (println "RESULT:")
+    (println "Top likers (<username> - <name>):")
+    (doseq  [liker top-likers-names] 
+            (do
+              (println (str "\"" (get liker 0) "\" - \"" (get liker 1) "\""))))
+    ))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [vec-args (vec args)
+        args-number (count vec-args)
+        user-name (if (<= 1 args-number) (get vec-args 0))
+        access-token (if (<= 2 args-number) (get vec-args 1))]
+    (println "args: " vec-args)
+    (println "args-number: " args-number)
+    (println "user-name: " user-name)
+    (println "access-token: " access-token)
+    (if (= 1 args-number) (if (= "help" (get vec-args 0)) (do
+        (println "")
+        (println "Usage:")
+        (println "lein run <Username> <AccessToken>")
+        (println "")
+        (println "Example:")
+        (println "lein run \"louiethelowe\" \"a5c9da88ad44ffce697b62d3a264f648848ab2031eeea91814ec9b75e44b8320\"")
+        )))
+    (if  (<= 2 args-number) (print-top-likers user-name 10 access-token))
+    (println "")
+    (println (str "DONE. Made " (:requests-count (deref app-state)) " requests."))))
